02ac334072a44865bef86ed4cdf24a0e7fe00a10
berchak2021 <91181604+berchak2021@users.noreply.github.com>
2022-12-13 14:59:35 +0200

    Add files via upload

---
+++ MoonFood/MoonFood_Utility/EmailSender.cs
@@ -0,0 +1,20 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonFood_Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MoonFood/MoonFood_Utility/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MoonFood_Utility
+{
+	public class StripeSettings
+	{
+		public string SecretKey { get; set; }
+		public string PublishableKey { get; set; }
+	}
+}
